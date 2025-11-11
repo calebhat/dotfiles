a31e702bd17af326507c54fdd1f866e270033dda
calebhat <hatajc@gmail.com>
2025-11-11 16:03:20 -0600

    prevent dev_term.sh from closing the terminal if theres an error

--- hypr/.config/hypr/scripts/dev_term.sh
+++ hypr/.config/hypr/scripts/dev_term.sh
@@ -2,5 +2,5 @@
 TARGET_DIR="$HOME/elixir/shophawk"
 
 ghostty --title=DEV_TERM -e bash -c "
-  cd '$TARGET_DIR' && mix phx.server && exec \"\$SHELL\";
+  cd '$TARGET_DIR' && mix phx.server; exec \"\$SHELL\";
 "
