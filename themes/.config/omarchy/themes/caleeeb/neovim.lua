5c837adfb6a1b8f03d7f9f3af9824068229a62f3
Caleb Hataj <hatajc@gmail.com>
2025-11-17 20:50:14 -0600

    added theme and changed border radius

---
+++ themes/.config/omarchy/themes/caleeeb/neovim.lua
@@ -0,0 +1,15 @@
+return {
+	{
+		"scottmckendry/cyberdream.nvim",
+		priority = 1000,
+		opts = {
+			transparent = false,
+		},
+	},
+	{
+		"LazyVim/LazyVim",
+		opts = {
+			colorscheme = "cyberdream",
+		},
+	},
+}
