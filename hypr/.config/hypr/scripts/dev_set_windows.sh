5f85f316c04d2c6cc9f07df1a33638adcc9df04b
calebhat <hatajc@gmail.com>
2025-11-25 07:40:49 -0600

    changed dev enviroment layout

--- hypr/.config/hypr/scripts/dev_set_windows.sh
+++ hypr/.config/hypr/scripts/dev_set_windows.sh
@@ -4,16 +4,8 @@ if hyprctl clients -j | jq -e '
 ' > /dev/null; then
     exit 0                     # nothing to do – windows are already open
 fi
-# Launch Neovim terminal
-~/.config/hypr/scripts/dev_neovim.sh &
-
-# Poll until DEV_NEOVIM window appears (using JSON output for precision)
-until hyprctl clients -j | jq -e '.[] | select(.title == "DEV_NEOVIM")' > /dev/null; do
-    usleep 10000  # 10ms micro-sleep to avoid high CPU usage in the loop
-done
-
 # Preselect bottom for horizontal split
-hyprctl dispatch layoutmsg preselect b
+#hyprctl dispatch layoutmsg preselect b
 
 # Launch Ghostty terminal
 ~/.config/hypr/scripts/dev_term.sh & \
@@ -23,6 +15,15 @@ until hyprctl clients -j | jq -e '.[] | select(.title == "DEV_TERM")' > /dev/nul
     usleep 10000
 done
 
+# Launch Neovim terminal
+~/.config/hypr/scripts/dev_neovim.sh &
+
+# Poll until DEV_NEOVIM window appears (using JSON output for precision)
+until hyprctl clients -j | jq -e '.[] | select(.title == "DEV_NEOVIM")' > /dev/null; do
+    usleep 10000  # 10ms micro-sleep to avoid high CPU usage in the loop
+done
+
+
 # Focus Neovim and expand downward
 hyprctl dispatch focuswindow title:^DEV_NEOVIM$
-hyprctl dispatch splitratio 0.25  # Adjust ratio as needed (e.g., 2 for 2:1, or exact 0.75 for 75%)
+hyprctl dispatch splitratio -0.25  # Adjust ratio as needed (e.g., 2 for 2:1, or exact 0.75 for 75%)
